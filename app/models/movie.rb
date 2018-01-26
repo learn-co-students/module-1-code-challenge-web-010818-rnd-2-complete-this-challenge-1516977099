72bacf65b656651e2da0a275232a3af74d99b10a
Vaibhav Patel <vp932712@gmail.com>
2018-01-26 10:23:11 -0500

    finished

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,65 @@ class Movie
     @@all
   end
 
+
+  def watchlistings
+     WatchListing.all.select do |obj|
+      obj.movie == self
+    end
+   end
+
+   def viewers
+     self.watchlistings.collect do |obj|
+       obj.viewers
+     end
+   end
+
+   def average_rating
+     total = 0
+     avg = 0
+     rating_arr = self.watchlistings.collect |obj|
+        obj.rating
+      end
+      rating_arr.each do |num|
+        total += num
+
+      end
+      avg =  total/rating_arr.length
+      avg
+   end
+
+
+   def self.highest_rated
+     higest = 0
+     higest_rated = ""
+     self.all.collect do |movie_instance|
+      if movie_instance.average_rating > higest
+        higest = movie_instance.average_rating
+        higest_rated = movie_instance
+        end
+      end
+    higest_rated
+    end
+
+
+
+
 end
+
+
+
+
+
+
+
+
+# Movie.all
+# returns an array of all movies
+# Movie#watchlistings
+# returns an array of all the watchlist objects that contain that movie
+# Movie#viewers
+# returns all of the viewers who added this movie to their watchlist
+# Movie#average_rating
+# returns the average of all ratings across all viewers watchlist ratings
+# Movie.highest_rated
+# should return the movie with the highest average rating across all the viewers watchlistings
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,29 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def add_to_watchlist(movie)
+    movie.each do |movie_instance|
+       WatchListing.new(self, movie_instance)
+     end
+  end
+
+  def rate_movie(movie, rating)
+    rate = WatchListing.all.find do |obj|
+      obj.viewer == self && obj.movie == movie
+    end
+    rate.rating = rating 
+
+  end
+
 end
+
+
+
+
+# Viewer.all
+# returns all of the viewers
+# Viewer#add_to_watchlist(movies)
+# this method should receive an array of one or more movie instances and add them to the viewer's watchlisting
+# Viewer#rate_movie(movie, rating)
+# given a movie on a viewer's watchlisting and a rating (a number between 1 and 5), assign the rating to the movie. Where should the rating that an individual viewer gives to an individual movie be stored?
