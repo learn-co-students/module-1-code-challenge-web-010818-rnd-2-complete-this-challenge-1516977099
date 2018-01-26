bdd1d99a831ca10a2df214e2edf0a7321b58965f
github username <github email address>
2018-01-26 10:38:45 -0500

    rn2 2, fara

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,29 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :rating, :viewers
 
   @@all = []
 
   def initialize(title)
     @title = title
+    @viewers = []
+    @ratings = []
     self.class.all << self
+  end
+
+  def watchlistings
+    Watchlisting.all.select{|watchlisting| watchlisting.include?(self) }
   end
 
   def self.all
     @@all
+  end
+
+  def average_rating
+    @ratings.reduce(:+)/@ratings.size
+  end
+
+  def self.highest_rated
+    Movie.all.map{|movie| movie.rating}.sort_by[0]
   end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,25 @@
 class Viewer
-  attr_accessor :username
+  attr_accessor :username, :watchlist
 
   @@all = []
 
   def initialize(username)
     @username = username
     self.class.all << self
+    @watchlist = []
+  end
+
+  def add_to_watchlist(movies)
+    movies.each{|movie| self.watchlist << movie}
+  end
+
+  def rate_movie(movie, rating)
+    Watchlisting.all.find{|listing| listing.title == movie.title}.rating = rating
+    movie.viewers << self.username
   end
 
   def self.all
     @@all
   end
-  
+
 end
