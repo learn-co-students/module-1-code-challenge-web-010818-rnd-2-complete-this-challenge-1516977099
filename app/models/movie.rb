b7ea06e3cab53265ccdc5ed5e0b55b50492c39f6
github username <github email address>
2018-01-26 10:36:30 -0500

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie
+  @@all=[]
   attr_accessor :title
-
-  @@all = []
 
   def initialize(title)
     @title = title
@@ -11,5 +10,46 @@ class Movie
   def self.all
     @@all
   end
+
+  def watchlistings
+    WatchListing.all.select do |wl|
+      wl.movie==self
+    end
+  end
+
+  def viewers
+    WatchListing.all.map do |wl|
+      if wl.movie==self
+        wl.viewer
+      end
+    end
+  end
+
+  def average_rating
+    ratings=[]
+    WatchListing.all.each do |wl|
+      ratings<<wl.rating
+    end
+    whole=ratings.inject{|sum,el| sum+el}
+    whole2=whole.to_f
+    whole2/ratings.length
+  end
+
+  def self.highest_rated
+    highest=0
+    movie=[]
+    WatchListing.all.find do |wl|
+      int=wl.movie.average_rating
+      if int>highest
+        highest=int
+        movie<<wl.movie
+      end
+    end
+    movie.last
+  end
+
+
+
+
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,7 +1,6 @@
 class Viewer
+  @@all=[]
   attr_accessor :username
-
-  @@all = []
 
   def initialize(username)
     @username = username
@@ -11,5 +10,19 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def add_to_watch_list(movies)
+    movies.each do |movie|
+      WatchListing.new(self.username,movie)
+    end
+  end
+
+  def rate_movie(movie,rating)
+    WatchListing.all.find do |m|
+      if m.viewer==self && m.movie==movie
+        m.rating=rating
+      end
+    end
+  end
+
 end
